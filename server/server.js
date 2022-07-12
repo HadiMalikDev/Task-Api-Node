ece55617060d96c89b414eb428cb85a2891ca8dc
Muhammad Hadi <hadimalikdev@gmail.com>
2022-07-12 20:49:10 +0500

    readding CORS implementation

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { graphqlHTTP } = require("express-graphql");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadSchemaSync } = require("@graphql-tools/load");
 const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
+const cors = require("cors");
 const graphqlResolver = require("./graphql/resolvers/index");
 const userRouter = require("./routers/user");
 
@@ -14,19 +15,7 @@ const schema = makeExecutableSchema({
   resolvers: graphqlResolver,
 });
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-
+server.use(cors());
 server.use(express.json());
 server.use(userRouter);
 server.use(
